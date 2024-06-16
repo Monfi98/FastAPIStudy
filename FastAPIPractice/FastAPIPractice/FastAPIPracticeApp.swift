23ca3143adfbcaa7aadcfb748b534717a6c0cb6c
shinseungjae <mok05247@naver.com>
2024-06-16 22:01:35 +0900

    프로젝트 생성

---
+++ FastAPIPractice/FastAPIPractice/FastAPIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastAPIPracticeApp.swift
+//  FastAPIPractice
+//
+//  Created by 신승재 on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FastAPIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RegisterView()
+        }
+    }
+}
---
+++ FastAPIPractice/FastAPIPractice/RegisterView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FastAPIPractice
+//
+//  Created by 신승재 on 6/16/24.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    RegisterView()
+}
